9ea1f6064c29ed24359df114dc483a2b619ae9c6
Robert Chen <robertchen@live.com>
2019-02-04 23:30:27 -0800

    Added client side score calculation

--- client/index.js
+++ client/index.js
@@ -36,8 +36,8 @@ const scoreboardItem = ({ username, score }) => `
 </tr>
 `;
 $(() => {
-  $.get("/problems").then(data => {
-    data.forEach((problem, i) => {
+  $.get("/problems").then(problemData => {
+    problemData.forEach((problem, i) => {
       const $problemElem = $(problemStatement(problem)).appendTo("#problems");
       if (i === 0) {
         $("#pid").val(problem.name);
@@ -54,62 +54,71 @@ $(() => {
         $("#pid").val(problem.name);
       });
     });
-  });
-  $.get("/grader/user").then(user => {
-    function reload() {
-      $.get("/grader").then(subs => {
-        $("#submissions").html(
-          subs
-            .filter(sub => sub.uid === user.uid)
-            .sort((a, b) => b.time - a.time)
-            .slice(0, 5)
-            .map(sub => submission(sub))
-            .join("\n")
-        );
+    
+    $.get("/grader/user").then(user => {
+      function reload() {
+        $.get("/grader").then(subs => {
+          $("#submissions").html(
+            subs
+              .filter(sub => sub.uid === user.uid)
+              .sort((a, b) => b.time - a.time)
+              .slice(0, 5)
+              .map(sub => submission(sub))
+              .join("\n")
+          );
 
-        const isUnique = (val, index, self) => self.indexOf(val) === index;
-        const idToName = {};
-        subs.forEach(sub => (idToName[sub.uid] = sub.username));
+          const isUnique = (val, index, self) => self.indexOf(val) === index;
+          const idToName = {};
+          subs.forEach(sub => (idToName[sub.uid] = sub.username));
 
-        const users = subs.map(sub => sub.uid).filter(isUnique);
+          const users = subs.map(sub => sub.uid).filter(isUnique);
 
-        const calculateScore = uid => {
-          const correctTimes = {};
-          subs
-            .filter(sub => sub.status === "OK")
-            .sort((a, b) => a.time - b.time)
-            .forEach(sub => {
-              if (correctTimes[sub.problem] == undefined) {
-                correctTimes[sub.problem] = sub.time;
-              }
-            });
-          const correct = Object.keys(correctTimes).length;
+          const calculateScore = uid => {
+            let score = 0;
+            
+            const problemToConfig = {};
+            problemData.forEach(problem => problemToConfig[problem.name] = problem.config);
+            
+            const correctTimes = {};
+            subs
+              .filter(sub => sub.status === "OK")
+              .sort((a, b) => a.time - b.time)
+              .forEach(sub => {
+                if (correctTimes[sub.problem] == undefined) {
+                  correctTimes[sub.problem] = sub.time;
+                  
+                  score += problemToConfig[sub.problem].value;
+                }
+              });
+            const correct = Object.keys(correctTimes).length;
 
-          const incorrect = subs
-            .filter(sub => sub.uid === uid)
-            .filter(sub => sub.status !== "OK" && sub.status !== "GRADING")
-            .filter(
-              sub =>
-                correctTimes[sub.problem] === undefined ||
-                sub.time < correctTimes[sub.problem]
-            ).length;
+            const incorrect = subs
+              .filter(sub => sub.uid === uid)
+              .filter(sub => sub.status !== "OK" && sub.status !== "GRADING")
+              .filter(
+                sub =>
+                  correctTimes[sub.problem] === undefined ||
+                  sub.time < correctTimes[sub.problem]
+              )
+              .forEach(sub => score -= problemToConfig[sub.problem].penalty);
 
-          return {
-            username: idToName[uid],
-            score: 50 * correct - 5 * incorrect
+            return {
+              username: idToName[uid],
+              score
+            };
           };
-        };
 
-        $("#scoreboard").html(
-          users
-            .map(calculateScore)
-            .sort((a, b) => b.score - a.score)
-            .map(sub => scoreboardItem(sub))
-            .join("\n")
-        );
-      });
-    }
-    setInterval(reload, 1000);
-    reload();
+          $("#scoreboard").html(
+            users
+              .map(calculateScore)
+              .sort((a, b) => b.score - a.score)
+              .map(sub => scoreboardItem(sub))
+              .join("\n")
+          );
+        });
+      }
+      setInterval(reload, 1000);
+      reload();
+    });
   });
 });
