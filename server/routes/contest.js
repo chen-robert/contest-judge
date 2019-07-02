24855a149b905ab353955c16ccf48f8a043b7ba7
Robert Chen <robertchen@live.com>
2019-07-02 15:12:53 -0700

    Refactored /contest ejs files

--- server/routes/contest.js
+++ server/routes/contest.js
@@ -4,13 +4,13 @@ const { renderWithPopups } = require(__rootdir + "/server/util");
 const router = require("express").Router();
 
 router.get("/", (req, res) =>
-  renderWithPopups(req, res, "pages/index", {
+  renderWithPopups(req, res, "pages/contest/index", {
     problems: loadProblems()
   })
 );
 
 router.get("/scoreboard", (req, res) =>
-  renderWithPopups(req, res, "pages/scoreboard", {})
+  renderWithPopups(req, res, "pages/contest/scoreboard")
 );
 
 module.exports = router;
