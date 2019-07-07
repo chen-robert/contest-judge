057571753c8c55256af14d2b814c633e2b23fe17
Robert Chen <robertchen@live.com>
2019-07-08 06:47:47 +0800

    Removed dropping tables

--- server/db/init.js
+++ server/db/init.js
@@ -8,8 +8,6 @@ const client = new Client({ connectionString });
 client.connect();
 
 const queries = [
-  "DROP TABLE users",
-  "DROP TABLE solves",
   "CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT not null, email TEXT not null, password TEXT not null, division TEXT not null)",
   "CREATE TABLE solves(user_id INTEGER not null, problem TEXT not null, status TEXT not null, time BIGINT not null)",
   "INSERT INTO users(username, email, password, division) VALUES('admin', '', '$2y$12$Y4NqIDHjqbkpc8ZoCzAKUukwK0vFWilDToV0bAnLwOcQ6ZKfDh56W', 'advanced') RETURNING *"
