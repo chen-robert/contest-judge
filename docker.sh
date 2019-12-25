a934eb4e15dd6729fd32095b2ece7529573a9e6f
Robert Chen <robertchen@live.com>
2019-12-24 17:17:54 -0800

    Updated docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
-while ! curl -s $CIRRUS_ENDPOINT > /dev/null
-do
-  echo "$(date) - waiting for cirrus endpoint at $CIRRUS_ENDPOINT"
-  sleep 1
-done
-echo "$(date) - connected to cirrus"
+#!/bin/bash
+
+if [ ${CIRRUS_ENDPOINT+x} ]; then
+  while ! curl -s $CIRRUS_ENDPOINT > /dev/null
+  do
+    echo "$(date) - waiting for cirrus endpoint at $CIRRUS_ENDPOINT"
+    sleep 1
+  done
+  echo "$(date) - connected to cirrus"
+else
+  echo "\$CIRRUS_ENDPOINT not defined, skipping checks"
+fi
 
 node index.js
